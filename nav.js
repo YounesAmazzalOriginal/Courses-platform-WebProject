77d7c0b82b9e1367921b2b54e2ed28376ee335dd
YounesAmazzalOriginal <younesamazzal4@gmail.com>
2026-04-14 19:07:17 +0100

    complete index & nav responsive + complete nav.js script

---
+++ nav.js
@@ -0,0 +1,26 @@
+const nav = document.querySelector("nav");
+const open_nav = document.querySelector(".open-nav");
+let active = true;
+
+function toggleNav() {
+    if (active) {
+        openNav();
+    }
+    else {
+        closeNav();
+    }
+}
+
+
+function openNav() {
+    active = false;
+    nav.style.transform = "translateX(0)";
+    open_nav.querySelector("i").classList.remove("fa-bars");
+    open_nav.querySelector("i").classList.add("fa-xmark");
+}
+function closeNav() {
+    active = true;
+    nav.style.transform = "translateX(-100%)";
+    open_nav.querySelector("i").classList.add("fa-bars");
+    open_nav.querySelector("i").classList.remove("fa-xmark");
+}
